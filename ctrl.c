d3cb410c7fe7d473fba4d0d8caa2065b8e93427b
Victor Lin <vlin5@stuy.edu>
2018-12-13 22:06:40 -0500

    Update ctrl.c

--- ctrl.c
+++ ctrl.c
@@ -34,7 +34,7 @@ void delete(){
 		int file = open("story.txt",O_RDONLY);
 		if(file==-1) printf("%s\n",strerror(errno));
 		else{
-			char * story = calloc(10000,1);
+			char * story;
 			read(file,story,10000);
 			printf("%s\n",story);
 			close(file);
@@ -49,7 +49,7 @@ void view(){
   int file = open("story.txt",O_RDONLY);
   if(file==-1) printf("%s\n",strerror(errno));
   else{
-  	  char * story = calloc(10000,1);
+  	  char * story;
 	  read(file,story,10000);
 	  printf("%s\n",story);
 	  close(file);
@@ -77,10 +77,10 @@ void edit(){
 				char * data = shmat(shmid,0,0);
 				printf("previously written data: %s\n",data);
 				printf("type in the next line of the story: ");
-				char * input = calloc(10000,1);
+				char * input;
 				fgets(input,10000,stdin);
 				strtok(input,"\n");
-				write(file,input,10000);
+				write(file,input,strlen(input)+1);
 				strcpy(data,input);
 			}
 		}
